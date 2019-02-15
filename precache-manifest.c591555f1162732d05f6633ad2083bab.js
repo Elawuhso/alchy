18d9eb9444724e0e70d2bec3a844feceb6a2b4bd
Matthias <matthias@f-renner.de>
2019-02-15 20:29:53 +0100

    init

---
+++ precache-manifest.c591555f1162732d05f6633ad2083bab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e51b4963bc27e2c8a2c",
+    "url": "/static/css/main.81c7e0d6.chunk.css"
+  },
+  {
+    "revision": "1e51b4963bc27e2c8a2c",
+    "url": "/static/js/main.1e51b496.chunk.js"
+  },
+  {
+    "revision": "4e5ce6c119e6075178b5",
+    "url": "/static/js/1.4e5ce6c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05ff81711bb5e2baec5dc4151a952d23",
+    "url": "/index.html"
+  }
+];
